5b5e12d8e33710b7554c88e2211e680e334ceef4
唐书 <leping.wlp@alibaba-inc.com>
2018-06-07 17:05:44 +0800

    增加动态加载示例

--- demo/TreeDemo.jsx
+++ demo/TreeDemo.jsx
@@ -5,6 +5,50 @@ import Menu from 'uxcore-menu';
 
 const { TreeNode, DropdownTreeNode } = Tree;
 
+function generateTreeNodes(treeNode) {
+  const arr = [];
+  const key = treeNode.props.eventKey;
+  for (let i = 0; i < 3; i++) {
+    arr.push({ name: `leaf ${key}-${i}`, key: `${key}-${i}` });
+  }
+  return arr;
+}
+
+function setLeaf(treeData, curKey, level) {
+  const loopLeaf = (data, lev) => {
+    const l = lev - 1;
+    data.forEach((item) => {
+      if ((item.key.length > curKey.length) ? item.key.indexOf(curKey) !== 0 :
+        curKey.indexOf(item.key) !== 0) {
+        return;
+      }
+      if (item.children) {
+        loopLeaf(item.children, l);
+      } else if (l < 1) {
+        item.isLeaf = true;
+      }
+    });
+  };
+  loopLeaf(treeData, level + 1);
+}
+
+function getNewTreeData(treeData, curKey, child, level) {
+  const loop = (data) => {
+    if (level < 1 || curKey.length - 3 > level * 2) return;
+    data.forEach((item) => {
+      if (curKey.indexOf(item.key) === 0) {
+        if (item.children) {
+          loop(item.children);
+        } else {
+          item.children = child;
+        }
+      }
+    });
+  };
+  loop(treeData);
+  setLeaf(treeData, curKey, level);
+}
+
 const Demo = React.createClass({
   propTypes: {
     multiple: React.PropTypes.bool,
@@ -20,11 +64,24 @@ const Demo = React.createClass({
       expandedKeys: ['0-0-0-key'],
       autoExpandParent: true,
       // checkedKeys: ['0-0-0-0-key', '0-0-1-0-key', '0-1-0-0-key'],
-      checkedKeys: ['0-0-key'],
+      checkedKeys: [],
       checkStrictlyKeys: {},
       selectedKeys: [],
       gData,
+      treeData: [],
     };
+  },
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        treeData: [
+          { name: 'pNode 01', key: '0-0' },
+          { name: 'pNode 02', key: '0-1' },
+          { name: 'pNode 03', key: '0-2', isLeaf: true },
+        ],
+        checkedKeys: ['0-0'],
+      });
+    }, 100);
   },
   onDragStart(info) {
     console.log('start', info);
@@ -96,6 +153,17 @@ const Demo = React.createClass({
       checkedKeys,
     });
   },
+  onLoadData(treeNode) {
+    console.log('load data...');
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const treeData = [...this.state.treeData];
+        getNewTreeData(treeData, treeNode.props.eventKey, generateTreeNodes(treeNode), 2);
+        this.setState({ treeData });
+        resolve();
+      }, 500);
+    });
+  },
   onCheckStrictly(checkedKeys, ...args /* extra */) {
     console.log('onCheckStrictly');
     console.log(args);
@@ -138,6 +206,19 @@ const Demo = React.createClass({
         return <TreeNode key={item.key} title={item.title} />;
       });
     };
+    const loopLoading = (data) => {
+      return data.map((item) => {
+        if (item.children) {
+          return <TreeNode title={item.name} key={item.key}>{loopLoading(item.children)}</TreeNode>;
+        }
+        return (
+          <TreeNode title={item.name} key={item.key} isLeaf={item.isLeaf}
+            disabled={item.key === '0-0-0'}
+          />
+        );
+      });
+    };
+    const treeNodes = loopLoading(this.state.treeData);
     // -----
     const menu = (<Menu>
       <Menu.Item>
@@ -211,6 +292,14 @@ const Demo = React.createClass({
           selectedKeys={this.state.selectedKeys}
         >
           {loop(gData)}
+        </Tree>
+        <h2>Dynamic Render</h2>
+        <Tree
+          onSelect={this.onSelect}
+          checkable onCheck={this.onCheck} checkedKeys={this.state.checkedKeys}
+          loadData={this.onLoadData}
+        >
+          {treeNodes}
         </Tree>
         <h2>checkStrictly</h2>
         <Tree
